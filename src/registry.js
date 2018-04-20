edf85cf661e8d18086b762162a3fc077de312939
Joseph Spira <spira.joe214@gmail.com>
2018-04-20 09:51:33 -0400

    Made initial ES6 changes

--- src/registry.js
+++ src/registry.js
@@ -1,16 +1,12 @@
 var Registry = {
-  registry: [],
-  register: function(entity) {
-    this.registry.push(entity);
-  },
-
-  entityFor: function(name) {
-    for (var i = 0; i < this.registry.length; i++) {
-      var entity = this.registry[i];
-
-      if (entity.handles(name)) {
-        return entity;
-      }
+    registry: [],
+    register: entity => this.registry.push(entity),
+    entityFor: name => {
+        for (let i of this.registry.length) {
+            var entity = i;
+            if (entity.handles(name)) {
+                return entity;
+            }
+        }
     }
-  }
 }
